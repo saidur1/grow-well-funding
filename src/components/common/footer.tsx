8d565159496b8ad1972245afbc13239e67abe441
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-10-11 10:10:12 +0600

    developed: footer

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,5 +1,72 @@
+import { navLinks } from "@/data";
+import { Instagram, Linkedin } from "lucide-react";
+import Link from "next/link";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="py-[50px] bg-primary-main">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-3 sectionContainer">
+        <div className="space-y-3">
+          <h1 className="text-white text-[20px] font-nunito font-semibold">
+            About Us
+          </h1>
+          <div>
+            <p className="text-[14px] text-white">
+              Here at Growwell, we pride ourselves on providing service that is
+              efficient, transparent, and comprehensive. Our team of experts
+              works for you to rapidly approve your application and connect you
+              to the financial boost you need for your next chapter. Work with
+              us today for the opportunity to make your business simpler, more
+              efficient, and ultimately more profitable
+            </p>
+          </div>
+        </div>
+        <div className="mx-auto space-y-3">
+          <h1 className="text-white text-[20px] font-nunito font-semibold">
+            Quick Links
+          </h1>
+          <div className="flex flex-col gap-y-3">
+            {navLinks.map(({ href, id, name }) => (
+              <Link
+                key={id}
+                className="text-white/60 hover:text-white transition duration-300"
+                href={href}
+              >
+                {name}
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className="mx-auto space-y-3">
+          <h1 className="text-white text-[20px] font-nunito font-semibold">
+            Contact Info
+          </h1>
+          <div className="text-white/60 space-y-2">
+            <h4>Email: info@growwellfunding.com</h4>
+            <h4>Phone: 845-307-4189</h4>
+            <h4>Fax: 800-816-5191</h4>
+          </div>
+        </div>
+        <div className="mx-auto space-y-3">
+          <h1 className="text-white text-[20px] font-nunito font-semibold">
+            Follow Us
+          </h1>
+          <div className=" flex items-center gap-x-3">
+            <a href="#">
+              <Instagram className="text-white/60 hover:text-white transition-colors duration-300" />
+            </a>
+            <a href="#">
+              <Linkedin className="text-white/60 hover:text-white transition-colors duration-300" />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <p className="text-white/80 text-center mt-[50px]">
+        © 2023 Growwell Funding All rights reserved.
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/ui/info-card.tsx
+++ src/components/ui/info-card.tsx
@@ -14,11 +14,11 @@ const InfoCard = ({ side, title, description, imgSrc }: Props) => {
     <section className="sectionContainer">
       <div
         className={cn(
-          "flex items-center gap-x-2",
+          "flex  items-center gap-x-2",
           side === "right" ? "flex-row-reverse" : ""
         )}
       >
-        <div className="flex-initial">
+        <div className="flex-initial hidden md:block">
           <Image
             src={imgSrc}
             height={400}
@@ -30,7 +30,9 @@ const InfoCard = ({ side, title, description, imgSrc }: Props) => {
         <div className="flex-1">
           <h1 className="text-primary-main text-[26px]">{title}</h1>
           <p className="text-[17px] text-primary-gray mt-4">{description}</p>
-          <ApplyButton />
+          <div className="mt-3">
+            <ApplyButton />
+          </div>
         </div>
       </div>
     </section>
