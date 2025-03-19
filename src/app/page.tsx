f691de5e3e207ccec82c571f148065e31480c910
saidur1 <saidur.rahman@live.com>
2025-03-19 17:21:20 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
-
 export default function Home() {
   return (
     <>
       <div className="pb-[100px] pt-[80px]" id="home">
-      <div style="margin: 0; padding: 0; text-align: center;">
-    <div style="position: absolute; top: 30px; width: 100%; font-size: 24px; font-weight: bold;">
-        Attention Growwellfunding.com: <span style="color: red;">Your website hosting has been suspended</span> due to non-payment. Please renew your hosting plan immediately to restore access. If not renewed, <span style="color: red; text-decoration: underline;">all website data will be permanently deleted</span> from our server.
-    </div>
-</div>
+        <div style={{ margin: 0, padding: 0, textAlign: "center" }}>
+          <div style={{ position: "absolute", top: 30, width: "100%", fontSize: 24, fontWeight: "bold" }}>
+            Attention <span style={{ color: "red" }}>Growwellfunding.com</span>: Your website hosting has been suspended due to non-payment. 
+            Please renew your hosting plan immediately to restore access. If not renewed,{" "}
+            <span style={{ color: "red", textDecoration: "underline" }}>all website data will be permanently deleted</span> from our server.
+          </div>
+        </div>
       </div>
     </>
   );
